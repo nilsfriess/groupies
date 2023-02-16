8e3b1cccea523097c002783348b1e676d14430bc
Nils Friess <nils.friess@gmail.com>
2023-02-16 21:16:15 +0100

    Show answers to additional questions in answers modal

--- src/components/AnswersModal.vue
+++ src/components/AnswersModal.vue
@@ -3,6 +3,7 @@ const props = defineProps({
   answers: Array,
   workshops: Array,
   workshopDescription: String,
+  additionalQuestions: Array,
 })
 
 const choices = (answer) => {
@@ -11,6 +12,12 @@ const choices = (answer) => {
       return props.workshops[index].name
     })
     .join(', ')
+}
+
+function answersForOption(questionIndex, optionIndex) {
+  return props.answers.filter((answer) => {
+    return answer.additionalQuestionAnswers[questionIndex][optionIndex]
+  })
 }
 
 defineEmits(['close'])
@@ -29,7 +36,7 @@ defineEmits(['close'])
         Erhaltene Antworten
       </header>
       <figure>
-        <table>
+        <table role="grid">
           <thead>
             <tr>
               <th>#</th>
@@ -48,6 +55,51 @@ defineEmits(['close'])
           </tbody>
         </table>
       </figure>
+
+      <section v-if="additionalQuestions && additionalQuestions.length > 0">
+        <h4>Antworten auf zusätzliche Fragen</h4>
+        <template v-for="(question, questionIndex) in additionalQuestions">
+          <h5>{{ question.question }}</h5>
+          <div v-for="(option, optionIndex) in question.options" class="option">
+            <b>
+              {{ option }}
+            </b>
+            <table
+              v-if="answersForOption(questionIndex, optionIndex).length > 0"
+              role="grid"
+            >
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Name</th>
+                  <th>Institution</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  v-for="(answer, answerIndex) in answersForOption(
+                    questionIndex,
+                    optionIndex
+                  )"
+                >
+                  <td>{{ answerIndex + 1 }}</td>
+                  <td>{{ answer.name }}</td>
+                  <td>{{ answer.institution }}</td>
+                </tr>
+              </tbody>
+            </table>
+            <p v-else>Keine Eintragungen für diese Option.</p>
+          </div>
+        </template>
+      </section>
     </article>
   </dialog>
 </template>
+
+<style scoped>
+.option {
+  border-left: 1px solid var(--primary);
+  padding-left: 1em;
+  margin-bottom: 2em;
+}
+</style>
